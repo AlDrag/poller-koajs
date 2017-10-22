872907c9613165761ef106521da45804df558270
Alan Dragicevich <aldragicevich@gmail.com>
2017-10-22 14:58:37 +1300

    Knexfile connect to production DB

--- knexfile.js
+++ knexfile.js
@@ -39,9 +39,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
