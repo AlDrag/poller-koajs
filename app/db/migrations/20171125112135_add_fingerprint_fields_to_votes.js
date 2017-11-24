fc2fee1430d2f5da926567316e5fdd028021de61
Alan Dragicevich <aldragicevich@gmail.com>
2017-11-25 11:55:01 +1300

    Added IPAddress and UserAgent fields to votes

---
+++ app/db/migrations/20171125112135_add_fingerprint_fields_to_votes.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.table('votes', (t) => {
+        knex('votes').w
+
+        t.string('ip_address').notNull().defaultTo(0);
+        t.string('user-agent').notNull().defaultTo(0);
+        t.string('ip_address').notNull().alter();
+        t.string('user-agent').notNull().alter();
+        t.index(['ip_address']);
+    });
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.table('votes', (t) => {
+        t.dropIndex(['ip_address']);
+        t.dropColumn('ip_address');
+        t.dropColumn('user-agent');
+    });
+};
